caf3d8bb1be9a5743d8536b6a0b751f62fcec6a0
Bilawal Khan <63578253+ibilawalkhan@users.noreply.github.com>
2023-09-21 00:43:47 +0500

    Update index.js

--- SocialMediaWebApp/server/index.js
+++ SocialMediaWebApp/server/index.js
@@ -24,6 +24,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors(
+  {
+    origin: ['https://deploy-mern-1whq.vercel.app'],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+))
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -56,10 +63,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect('mongodb+srv://Bilawal:1234@cluster0.fzvzfa0.mongodb.net/test?retryWrites=true&w=majority)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
